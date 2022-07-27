44e01f8fc3c7a3480e11f87330dfc8f8a5157eb1
cmarshak <charles.marshak@gmail.com>
2022-07-27 11:50:13 -0700

    upload data

--- run_pipeline.py
+++ run_pipeline.py
@@ -55,9 +55,21 @@ def classify_planet_img(planet_id):
     return out_class_dir
 
 
+def upload_data():
+   out_dir = Path(f'upload_data_notebook/')
+   out_dir.mkdir(exist_ok=True, parents=True)
+
+   out_notebook_path = out_dir / f'upload_data.ipynb'
+
+   pm.execute_notebook('2_upload_data_to_s3.ipynb',
+                       output_path=out_notebook_path,
+                       )
+   return out_notebook_path
+
+
 @click.command()
 @click.option('--download_planet_img', default=False, help='Download Planet Imgs Locally')
-@click.option('--use_local_chips', default=True, help='Classify local chips or all Ids in DB')
+@click.option('--use_local_chips', default=True, help='Classify only local chips or download all onto disk')
 def main(download_planet_img, use_local_chips):
     planet_ids = get_planet_ids_from_db()
     if use_local_chips:
@@ -70,6 +82,8 @@ def main(download_planet_img, use_local_chips):
         return out_data
 
     _ = list(map(generate_validation_dataset, tqdm(planet_ids, desc='Generating Validation Dataset')))
+
+    upload_data()
 
 
 if __name__ == '__main__':
